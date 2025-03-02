afe48042deca04294077672bda30f5b78f71fcdb
Thiwanka836 <nayanajiththiwanka194@gmail.com>
2025-03-02 12:54:46 +0530

    Update queen.js

--- queen.js
+++ queen.js
@@ -25,7 +25,7 @@
 //*
 //Base by Janith Rashmika
 //GitHub: @Mrrashmika
-//WhatsApp: +94717775628
+//WhatsApp: +94740938105
 //Want more free bot scripts? Subscribe to my YouTube channel: https://youtube.com/@gamingrash2006
 //   * Created By GitHub: Mrrashmika
 //   * Credit To Janith Rashmika
